eee09d241386f71c26d420130d39c6240d832336
Vadym Grabovyi <hraboviyvadim@gmail.com>
2018-03-25 12:55:49 +0300

    refactor

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from './../Header/Header';
+import Header from '../Header/Header';
+import Login from '../Auth/Login/Login';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Login />
       </div>
     );
   }
